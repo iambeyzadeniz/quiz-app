10f08e878915f7cf35a1f4bec228fdc58788c0a4
iambeyzadeniz <denizbeyzatr@gmail.com>
2023-12-13 14:40:42 +0300

    finished

--- src/components/App.js
+++ src/components/App.js
@@ -10,10 +10,12 @@ import Question from "./Question";
 import NextButton from "./NextButton";
 import Progress from "./Progress";
 import FinishScreen from "./FinishScreen";
+import Footer from "./Footer";
+import Timer from "./Timer";
 
 
 
-
+const SECS_PER_QUESTION = 30;
 const initialState = {
   questions: [],
   // "loading" , "error" ,"ready" ,"active" ,"finished"
@@ -21,11 +23,15 @@ const initialState = {
   index: 0,
   answer: null,
   points: 0,
-  highscore: 0
+  highscore: 0,
+  secondsRemaining: null,
 
 }
 
 function reducer(state, action) {
+  // console.log(state.status);
+  // console.log("type", action);
+
   switch (action.type) {
     case "dataReceived":
       return {
@@ -41,7 +47,7 @@ function reducer(state, action) {
     case "start":
       return {
         ...state,
-        status: "active"
+        status: "active", secondsRemaining: state.questions.length * SECS_PER_QUESTION
       };
     case "newAnswer":
       const question = state.questions.at(state.index);
@@ -70,6 +76,12 @@ function reducer(state, action) {
     //   status:"ready",
     // }
 
+    case "tick":
+      return {
+        ...state, secondsRemaining: state.secondsRemaining - 1,
+        status: state.secondsRemaining === 0 ? "finished" : state.status,
+      }
+
     default:
       throw new Error('Beklenmeyen durum!');
   }
@@ -77,8 +89,9 @@ function reducer(state, action) {
 // anladığım kadarıyla state : status ve questions vs gibi durumları barındırırken... action : type ve payload döndürüyor ,type ve payload da dispatch ile sevkiyatı yapılıyor. 
 export default function App() {
   // önceki durum const [state, dispatch] = useReducer(reducer, initialState); state yerine {question,status}
-  const [{ questions, status, index, answer, points, highscore }, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status, index, answer, points, highscore, secondsRemaining }, dispatch] = useReducer(reducer, initialState);
   const numQuestions = questions.length;
+
   const sumPoints = questions.reduce((prev, cur) => prev + cur.points, 0);
   useEffect(function () {
     fetch("http://localhost:9000/questions")
@@ -101,7 +114,10 @@ export default function App() {
           <>
             <Progress index={index} numQuestion={numQuestions} points={points} sumPoints={sumPoints} />
             <Question question={questions[index]} dispatch={dispatch} answer={answer} />
-            <NextButton dispatch={dispatch} answer={answer} index={index} numQuestions={numQuestions} />
+            <Footer>
+              <Timer dispatch={dispatch} secondsRemaining={secondsRemaining} />
+              <NextButton dispatch={dispatch} answer={answer} index={index} numQuestions={numQuestions} />
+            </Footer>
           </>
         }
         {status === "finished" && (<FinishScreen points={points} sumPoints={sumPoints} highscore={highscore} dispatch={dispatch} />)}
